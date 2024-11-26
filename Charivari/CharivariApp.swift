e17692a616f13482f6176580c016a24bd810189f
William Leroux <2251513@etu.cegep-lanaudiere.qc.ca>
2024-11-26 16:21:59 -0500

    Initial Commit

---
+++ Charivari/CharivariApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharivariApp.swift
+//  Charivari
+//
+//  Created by William Leroux on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct CharivariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CharivariTests/CharivariTests.swift
@@ -0,0 +1,17 @@
+//
+//  CharivariTests.swift
+//  CharivariTests
+//
+//  Created by William Leroux on 2024-11-26.
+//
+
+import Testing
+@testable import Charivari
+
+struct CharivariTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
